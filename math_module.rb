c502816cfd71cb9beb6b14aba7690e43bb3db265
veezdev <luthfizonedev@gmail.com>
2026-06-17 19:47:09 +0700

    math module

---
+++ math_module.rb
@@ -0,0 +1,12 @@
+# Math module
+# In ruby, math is a module that stores mathematical functions and constants. It is a built-in module that can be used to perform various mathematical operations, such as trigonometric functions, logarithmic functions, and more.
+
+# The Math module provides a collection of methods for performing mathematical calculations. 
+# It includes methods for basic arithmetic operations, as well as more advanced functions like trigonometry, logarithms, and exponentiation.
+
+# The Math module is included in the Ruby core library, so you don't need to require any additional libraries to use it. 
+# You can simply call the methods in the Math module by prefixing them with "Math.".
+
+puts Math.sqrt(16) # Output: 4.0
+puts Math.exp(2.0) # Output: 7.38905609893065
+puts Math.log(10) # Output: 2.302585092994046
